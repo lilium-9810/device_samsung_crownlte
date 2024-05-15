5d6680c16c2e27132f1f69ba9f368a6ebb6a8e95
Cyderxxv <phamnguyengiakhiem@gmail.com>
2024-05-16 00:11:28 +0700

    crownlte: U bringup

--- lineage_crownlte.mk
+++ lineage_crownlte.mk
@@ -30,10 +30,10 @@ TARGET_SCREEN_WIDTH := 1440
 TARGET_BOOT_ANIMATION_RES := 1440
 
 ## Inherit some common AOSP stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := aosp_crownlte
+PRODUCT_NAME := lineage_crownlte
 PRODUCT_DEVICE := crownlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N960F
